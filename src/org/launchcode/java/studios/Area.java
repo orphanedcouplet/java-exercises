74b1147f6bc0a8f91377b6b9da95da3abdaa6f90
orphanedcouplet <oliverdayers@gmail.com>
2017-11-06 13:36:49 -0600

    completed studio 1 - Area

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -1,4 +1,15 @@
 package org.launchcode.java.studios;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of the circle?");
+        double radius = input.nextDouble();
+        double area = pi * radius * radius;
+        String areaStr = Double.toString(area);
+        System.out.printf(String.format("The area of the circle is %s", areaStr));
+    }
 }
